ef743012db7b40920bf1e04396e692dd573a5dc5
Duncan Bayne <dhgbayne@gmail.com>
2011-12-26 13:40:09 +1100

    Ooops.  Missed StumpWM configuration from mint_setup_user.sh

--- mint_setup_user.sh
+++ mint_setup_user.sh
@@ -4,6 +4,7 @@
 if [ ! -f ~/.xinitrc   ]; then ln -s $(pwd)/conf/x/xinitrc ~/.xinitrc           ; fi  
 if [ ! -f ~/.emacs     ]; then ln -s $(pwd)/conf/emacs/emacs ~/.emacs           ; fi
 if [ ! -d ~/.emacs.d   ]; then ln -s $(pwd)/conf/emacs/emacs.d ~/.emacs.d       ; fi
+if [ ! -f ~/.stumpwmrc ]; then ln -s $(pwd)/conf/stumpwm/stumpwmrc ~/.stumpwmrc ; fi  
 
 # Custom Bash setup, but only once (edit bash_extras to make changes)
 if grep -q "037b7c29-5804-43e2-8054-d1ebfb0f3293" ~/.bashrc ;
