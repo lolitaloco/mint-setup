a627a4e69c7c4cf5a515a6f8ebb90d75481d8bf9
Duncan Bayne <dhgbayne@gmail.com>
2012-11-04 10:10:20 +1100

    Extracting Emacs config to separate Git repo

--- mint_setup_user.sh
+++ mint_setup_user.sh
@@ -3,8 +3,6 @@ set -e
 
 echo Linking custom config
 if [ ! -f ~/.xinitrc   ]; then ln -s $(pwd)/conf/x/xinitrc ~/.xinitrc           ; fi  
-if [ ! -f ~/.emacs     ]; then ln -s $(pwd)/conf/emacs/emacs ~/.emacs           ; fi
-if [ ! -d ~/.emacs.d   ]; then ln -s $(pwd)/conf/emacs/emacs.d ~/.emacs.d       ; fi
 if [ ! -f ~/.stumpwmrc ]; then ln -s $(pwd)/conf/stumpwm/stumpwmrc ~/.stumpwmrc ; fi  
 
 if grep -q "037b7c29-5804-43e2-8054-d1ebfb0f3293" ~/.bashrc ;
