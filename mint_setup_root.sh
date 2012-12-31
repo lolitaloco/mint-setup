69d23bdda886b66c234f5d28359f0f891d211720
Duncan Bayne <dhgbayne@gmail.com>
2012-12-31 16:01:39 +1100

    Sundry changes to complete Linux Mint 14 compatibility.

--- mint_setup_root.sh
+++ mint_setup_root.sh
@@ -1,36 +1,27 @@
 #!/bin/bash
 set -e
 
-# Update and upgrade from all repositories
+echo
+echo Update and upgrade from all repositories
 apt-get update -y
 apt-get upgrade -y
 
-# Downgrade to GCC 4.5
-apt-get -y purge gcc
-apt-get -y install gcc-4.5 g++-4.5 build-essential libc6-dev-i386 gcc-4.5-multilib
-if [ -e /usr/bin/gcc ]; then rm -f /usr/bin/gcc ; fi
-if [ -e /usr/bin/g++ ]; then rm -f /usr/bin/g++ ; fi
-ln /usr/bin/gcc-4.5 /usr/bin/gcc
-ln /usr/bin/g++-4.5 /usr/bin/g++ 
-
-# Install StumpWM and dependencies
+echo
+echo Install StumpWM and dependencies
 apt-get purge -y pulseaudio
 apt-get install -y stumpwm network-manager-gnome trayer cl-swank cl-clx-sbcl xloadimage alsa-utils
 cp -f $(pwd)/conf/stumpwm/stumpwm.desktop /usr/share/xsessions/stumpwm.desktop
 
-# Install node.js development stack
-apt-get install -y curl nodejs
-curl http://npmjs.org/install.sh | sh
-npm install -g coffee-script
-apt-get install -y mongodb
+echo
+echo Install miscellaneous stuff
+apt-get -y install emacs k3b ccrypt eclipse autoconf libusb-dev xclip pdftk gqview gimp gnome-rdp xtightvncviewer lyx latex2html lynx pwgen dosbox tofrodos gettext net-tools audacity lame php5-cli php5-curl gparted vice dia frotz inform wine curl xsane libxine1-ffmpeg git-core subversion shtool libxslt-dev libssl-dev libpq-dev inotify-tools libnotify-bin acidrip libsqlite3-dev libreadline-gplv2-dev openssh-server gitg nautilus chromium-browser libtool automake libav-tools postgresql
 
-# Install miscellaneous stuff
-apt-get -y install emacs k3b ccrypt eclipse autoconf libusb-dev xclip pdftk gqview gimp gnome-rdp xtightvncviewer lyx latex2html lynx pwgen dosbox tofrodos gettext net-tools audacity lame php5-cli php5-curl gparted vice dia frotz inform wine curl xsane libxine1-ffmpeg git-core subversion shtool libxslt-dev libssl-dev libpq-dev inotify-tools libnotify-bin acidrip libsqlite3-dev libreadline-gplv2-dev openssh-server gitg nautilus chromium-browser libtool automake libav-tools
-
-# Configure VICE
+echo
+echo Configure VICE
 cp -R $(pwd)/conf/vice/* /usr/lib/vice -R
 
-# Build SBCL from source, so we're using a good version with threading enabled
+echo
+echo Build SBCL from source, so we are using a good version with threading enabled
 apt-get -y install clisp
 rm -rf /tmp/sbcl
 mkdir /tmp/sbcl
@@ -43,7 +34,8 @@ INSTALL_ROOT=/usr/local sh install.sh
 popd
 rm -rf /tmp/sbcl
 
-# Make StumpWM use SBCL, not CLISP
+echo
+echo Make StumpWM use SBCL, not CLISP
 apt-get purge -y clisp
 if !(grep -q sbcl /etc/profile)
   then echo export LISP=sbcl >> /etc/profile
