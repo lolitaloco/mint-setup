daa00579baf30a4e9d07edc3efc1071271c47e70
Duncan Bayne <dhgbayne@gmail.com>
2013-01-10 19:52:54 +1100

    Copy paste :-(

--- mint_setup_root.sh
+++ mint_setup_root.sh
@@ -43,7 +43,7 @@ fi
 
 echo
 echo Installing Spotify
-deb http://repository.spotify.com stable non-free
+echo deb http://repository.spotify.com stable non-free | tee -a /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 apt-get update
 apt-get install spotify-client -y
