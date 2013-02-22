2581e41690f0ab10a1f0d59dea733f0fa6af3e08
Duncan Bayne <duncan.bayne@rea-group.com>
2013-02-22 15:25:50 +1100

    Fixed some raise-or-run weirdness, cleaned up whitespace, added a few git aliases.

--- conf/bash/bash_extras.sh
+++ conf/bash/bash_extras.sh
@@ -1,4 +1,4 @@
-export PATH=/var/lib/gems/1.8/bin:$HOME/personal/bin/:$HOME/bin:$PATH 
+export PATH=/var/lib/gems/1.8/bin:$HOME/personal/bin/:$HOME/bin:$PATH
 export GIT_EDITOR=emacs
 
 alias gs='git status'
@@ -9,5 +9,7 @@ alias ga='git add'
 alias gd='git diff --color'
 alias gl='git log'
 alias gc='git commit'
+alias gss='git stash save'
+alias gsp='git stash pop'
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
