fc72b65533027eac2783a31c12ec6775a44f90c3
Duncan Bayne <duncan.bayne@rea-group.com>
2013-09-04 19:01:12 +1000

    Removed redundant setup

--- install_rbenv.sh
+++ install_rbenv.sh
@@ -6,17 +6,6 @@ pushd ~
 rm -rf ~/.rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
-if grep -q "5ecd9684-b577-437a-afc8-ecfa85c85ad3" ~/.bash_profile;
-then
-    echo rbenv already initialized in ~/.bash_profile
-else
-    echo \# rbenv, added by install_rbenv.sh >> ~/.bash_profile
-    echo \# 5ecd9684-b577-437a-afc8-ecfa85c85ad3 >> ~/.bash_profile
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-    echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
-    echo >> ~/.bash_profile
-fi
-
 # initialize rbenv so we can use it without restarting the shell
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
