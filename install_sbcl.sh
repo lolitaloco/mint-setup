390aab8fd8481c8e886d73616d67df96e2144538
Duncan Bayne <dhgbayne@gmail.com>
2013-09-12 21:50:53 +1000

    Bumped to new version of SBCL.

--- install_sbcl.sh
+++ install_sbcl.sh
@@ -7,10 +7,11 @@ echo Build SBCL from source, so we are using a good version with threading enabl
 apt-get -y install clisp
 rm -rf /tmp/sbcl
 mkdir /tmp/sbcl
-cp $(pwd)/src/sbcl-1.0.54-source.tar.gz /tmp/sbcl
+cp $(pwd)/src/sbcl-1.1.11-source.tar.bz2 /tmp/sbcl
 pushd /tmp/sbcl
-tar -zxvf sbcl-1.0.54-source.tar.gz
-cd sbcl-1.0.54
+bunzip2 sbcl-1.1.11-source.tar.bz2
+tar -xvf sbcl-1.1.11-source.tar
+cd sbcl-1.1.11
 sh make.sh clisp
 INSTALL_ROOT=/usr/local sh install.sh
 popd
