0fd318037cdda67a17baff1aa73572c9d439b77a
Duncan Bayne <dhgbayne@fastmail.fm>
2013-10-21 16:32:53 +1100

    No longer using Google Code

--- install_anki.sh
+++ install_anki.sh
@@ -7,7 +7,7 @@ echo Installing Anki
 
 mkdir -p ~/mint_setup_tmp/anki
 pushd ~/mint_setup_tmp/anki
-wget https://anki.googlecode.com/files/anki-2.0.12.deb
+wget http://ankisrs.net/download/mirror/anki-2.0.14.deb
 sudo apt-get install -y python-sqlalchemy
 dpkg -i anki-2.0.12.deb
 popd
