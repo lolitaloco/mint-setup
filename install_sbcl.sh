b528f673937bb08ccef89898115c5b8cac86cf5f
Duncan Bayne <dhgbayne@gmail.com>
2014-03-02 13:06:47 +1100

    Upgrade to SBCL 1.1.16 to fix broken SLIME.

--- install_sbcl.sh
+++ install_sbcl.sh
@@ -5,14 +5,19 @@ echo
 echo Build SBCL from source, so we are using a good version with threading enabled
 
 apt-get -y install clisp
+
 rm -rf ~/mint_setup_tmp/sbcl
-mkdir ~/mint_setup_tmp/sbcl
-cp $(pwd)/src/sbcl-1.1.11-source.tar.bz2 ~/mint_setup_tmp/sbcl
+mkdir -p ~/mint_setup_tmp/sbcl
+curl "http://aarnet.dl.sourceforge.net/project/sbcl/sbcl/1.1.16/sbcl-1.1.16-source.tar.bz2" > sbcl-source.tar.bz2
+cp sbcl-source.tar.bz2 ~/mint_setup_tmp/sbcl
 pushd ~/mint_setup_tmp/sbcl
-bunzip2 sbcl-1.1.11-source.tar.bz2
-tar -xvf sbcl-1.1.11-source.tar
-cd sbcl-1.1.11
+bunzip2 sbcl-source.tar.bz2
+tar -xvf sbcl-source.tar
+cd sbcl-1.1.16
 sh make.sh clisp
 INSTALL_ROOT=/usr/local sh install.sh
 popd
+
+rm -f sbcl-source.tar.bz2
 rm -rf ~/mint_setup_tmp/sbcl
+apt-get purge -y clisp
