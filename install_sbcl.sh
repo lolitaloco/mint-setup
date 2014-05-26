80e45ad4d670cbdc8417832ab0f07b75969b731b
Duncan Bayne <duncan@bayne.id.au>
2014-05-26 20:03:10 +1000

    BUGFIX: now installs SBCL correctly (#39)

--- install_sbcl.sh
+++ install_sbcl.sh
@@ -8,12 +8,12 @@ apt-get -y install clisp
 
 rm -rf ~/mint_setup_tmp/sbcl
 mkdir -p ~/mint_setup_tmp/sbcl
-curl "http://aarnet.dl.sourceforge.net/project/sbcl/sbcl/1.1.16/sbcl-1.1.16-source.tar.bz2" > sbcl-source.tar.bz2
+curl "http://aarnet.dl.sourceforge.net/project/sbcl/sbcl/1.1.18/sbcl-1.1.18-source.tar.bz2" > sbcl-source.tar.bz2
 cp sbcl-source.tar.bz2 ~/mint_setup_tmp/sbcl
 pushd ~/mint_setup_tmp/sbcl
 bunzip2 sbcl-source.tar.bz2
 tar -xvf sbcl-source.tar
-cd sbcl-1.1.16
+cd sbcl-1.1.18
 sh make.sh clisp
 INSTALL_ROOT=/usr/local sh install.sh
 popd
