5caaf83be0307305277926b52b0403a7fabcc125
Duncan Bayne <dhgbayne@fastmail.fm>
2014-06-26 09:20:49 +1000

    Now uses pre-release Emacs 24.4 (24.3.91) to get nnimap fix.

--- install_emacs.sh
+++ install_emacs.sh
@@ -5,12 +5,14 @@ echo
 echo Downloading and installing Emacs from source
 echo
 
+apt-get install -y libtiff
+
 rm -rf ~/mint_setup_tmp/emacs
 mkdir ~/mint_setup_tmp/emacs
 pushd ~/mint_setup_tmp/emacs
-wget http://mirror.aarnet.edu.au/pub/gnu/emacs/emacs-24.3.tar.gz
-tar -zxvf emacs-24.3.tar.gz
-cd emacs-24.3
+wget ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-24.3.91.tar.xz
+tar -xvf emacs-24.3.91.tar.xz
+cd emacs-24.3.91
 ./configure
 make
 make install
