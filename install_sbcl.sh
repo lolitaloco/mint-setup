5840132cfcdd520f65ae74cdca184bfeff79b382
Duncan Bayne <duncan@bayne.id.au>
2015-01-22 15:45:21 +1100

    Just use the packaged SBCL as StumpWM really doesn't like the latest.

--- install_sbcl.sh
+++ install_sbcl.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 set -e
 
-echo
-echo Build SBCL from source, so we are using a good version with threading enabled
-
-apt-get -y install clisp
-
-rm -rf ~/mint_setup_tmp/sbcl
-mkdir -p ~/mint_setup_tmp/sbcl
-curl "http://aarnet.dl.sourceforge.net/project/sbcl/sbcl/1.2.2/sbcl-1.2.2-source.tar.bz2" > sbcl-source.tar.bz2
-cp sbcl-source.tar.bz2 ~/mint_setup_tmp/sbcl
-pushd ~/mint_setup_tmp/sbcl
-bunzip2 sbcl-source.tar.bz2
-tar -xvf sbcl-source.tar
-cd sbcl-1.2.2
-sh make.sh clisp
-INSTALL_ROOT=/usr/local sh install.sh
-popd
-
-rm -f sbcl-source.tar.bz2
-rm -rf ~/mint_setup_tmp/sbcl
-apt-get purge -y clisp
+apt-get install -y sbcl
