148cf5988670da73fe82fe0a95e1ca1dfdb8ca6a
Duncan Bayne <duncan@bayne.id.au>
2015-07-12 23:42:30 +1000

    Try not to kill Cinnamon during StumpWM installation

--- install_stumpwm.sh
+++ install_stumpwm.sh
@@ -8,7 +8,6 @@ cp -f $(pwd)/conf/stumpwm/sbcl.sh /etc/profile.d/sbcl.sh
 
 echo
 echo Install StumpWM and dependencies
-apt-get purge -y pulseaudio
 apt-get install -y stumpwm network-manager-gnome trayer cl-swank cl-clx-sbcl xloadimage alsa-utils
 cp -f $(pwd)/conf/stumpwm/stumpwm.desktop /usr/share/xsessions/stumpwm.desktop
 
