b6c7511b358e654df080763fef713b38135c734f
Duncan Bayne <duncan@bayne.id.au>
2015-07-12 23:39:46 +1000

    Fix Spotify installation

--- install_spotify.sh
+++ install_spotify.sh
@@ -7,4 +7,4 @@ echo Installing Spotify
 echo deb http://repository.spotify.com stable non-free | tee -a /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 apt-get update
-apt-get install spotify-client -y
+apt-get install spotify-client -y --force-yes
